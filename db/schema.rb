fa8580ed865d8ad79519265c2c80e0217af311ca
BorisTitarenko <specmohnat2309@gmail.com>
2019-07-20 10:28:06 +0300

    set depencies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_06_140823) do
+ActiveRecord::Schema.define(version: 2019_07_20_071537) do
 
   create_table "comments", force: :cascade do |t|
     t.string "username"
@@ -18,12 +18,25 @@ ActiveRecord::Schema.define(version: 2019_07_06_140823) do
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
